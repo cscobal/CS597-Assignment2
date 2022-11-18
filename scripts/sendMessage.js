816fda6250d373a71a0e7d58ec40bda568917c52
cscobal <cscobal@gmail.com>
2022-11-17 21:58:52 -0800

    Update sendMessage.js

--- scripts/sendMessage.js
+++ scripts/sendMessage.js
@@ -1,17 +1,30 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-const AWS = require('aws-sdk')
-const sns = new AWS.SNS();
+const AWS = require("aws-sdk");
+const ses = new AWS.SES();
 
-const sendMessage = async ({ phoneNumber, message }) => {
+const sendMessage = async ({ email, message, subject }) => {
   const params = {
-    Message: message,
-    PhoneNumber: phoneNumber
-  }
+    Destination: { /* required */
+    ToAddresses: [email]
+    },
+    Message: { /* required */
+    Body: { /* required */
+      Text: {
+       Data: message
+      }
+     },
+     Subject: {
+      Data: subject
+     }
+    },
+  Source: 'cscobal@gmail.com', /* required */
+  };
 
-  return sns.publish(params).promise()
-}
+  return ses.sendEmail(params).promise();
+};
 
-sendMessage({ phoneNumber: process.env.PHONE_NUMBER, message: 'Sending a message from SNS!'})
+
+sendMessage({ email: process.env.EMAIL, message: 'Sending a message from SES!', subject: "yo"})
   .then(() => console.log('Sent message successfully'))
   .catch((error) => console.log('Error sending SNS: ', error.message))
