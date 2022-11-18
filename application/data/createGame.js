87f54c942e4379843f7238a0d56525d85b4d9c8c
cscobal <cscobal@gmail.com>
2022-11-17 22:03:31 -0800

    Update createGame.js

--- application/data/createGame.js
+++ application/data/createGame.js
@@ -7,18 +7,27 @@ const sendMessage = require("./sendMessage");
 
 
 const createGame = async ({ creator, opponent, size }) => {
+  const vals = Array.from(Array(size), (_, i) => i + 1)
+
+  const dSum = [Array.from(Array((size * 2) + 1), () => new Array(size).fill(0)), Array.from(Array((size * 2) + 1), () => new Array(size).fill(0))]
+  dSum[0][size + 1] = vals 
+  dSum[1][size + 1] = Array.from(vals, i => -i)
+
   const params = {
-    TableName: "tic-tac-toe-game",
+    TableName: 'tic-tac-toe-game',
     Item: {
-      gameId: uuidv4().split('-')[0],
-      user1: creator,
-      user2: opponent.username,
+      gameId: '5b5ee7d8',
+      user1: 'userx',
+      user2: 'usero',
       size: size,
-      row: Array(size).fill(0),
-      col: Array(size).fill(0),
-      diag: Array(2).fill(0),
+      tttRow: Array(size).fill(0),
+      tttCol: Array(size).fill(0),
+      tttDiag: Array(2).fill(0),
       previousMoves: [],
-      lastMoveBy: creator
+      lastMoveBy: 'usero',
+      diagSum: dSum,
+      currentValue: [[['x'], vals], [['o'], Array.from(vals, i => -i)]],
+      currMove: [[-1, -1, '=']]
     }
   };
 
