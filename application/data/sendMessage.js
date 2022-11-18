e10694455bd46b35ab5fa83aca914d0e35b3f1fa
cscobal <cscobal@gmail.com>
2022-11-17 22:02:50 -0800

    Update sendMessage.js

--- application/data/sendMessage.js
+++ application/data/sendMessage.js
@@ -1,15 +1,27 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 const AWS = require("aws-sdk");
-const sns = new AWS.SNS();
+const ses = new AWS.SES();
 
-const sendMessage = async ({ phoneNumber, message }) => {
+const sendMessage = async ({ email, message, subject }) => {
   const params = {
-    Message: message,
-    PhoneNumber: phoneNumber
+    Destination: { /* required */
+    ToAddresses: [email]
+    },
+    Message: { /* required */
+    Body: { /* required */
+      Text: {
+       Data: message
+      }
+     },
+     Subject: {
+      Data: subject
+     }
+    },
+  Source: 'cscobal@gmail.com', /* required */
   };
 
-  return sns.publish(params).promise();
+  return ses.sendEmail(params).promise();
 };
 
 module.exports = sendMessage;
