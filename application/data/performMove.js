b1803b2db2e38dd203c8a6377b8f39b12424bd0b
cscobal <cscobal@gmail.com>
2022-11-18 01:39:01 -0800

    Update performMove.js
    Prevent changes to the game once someone wins

--- application/data/performMove.js
+++ application/data/performMove.js
@@ -29,8 +29,8 @@ const performMove = async ({ gameId, user, changedRow, changedCol }) => {
 
   const calcDiag = "tttDiag[1] + diagSum[0][" + diagVal + "][" + (changedRow - 1) + "]"
   
-  const rowInd = "tttRow[" + (changedCol - 1) + "]" 
-  const colInd = "tttCol[" + (changedRow - 1) + "]"
+  const rowInd = "tttRow[" + (changedRow - 1) + "]" 
+  const colInd = "tttCol[" + (changedCol - 1) + "]"
   
   const rowVal = "currentValue[0][1][" + (changedCol - 1) + "]"
   const colVal = "currentValue[0][1][" + (changedRow - 1) + "]"
@@ -38,6 +38,7 @@ const performMove = async ({ gameId, user, changedRow, changedCol }) => {
   const moveXCheck = [...attemptMove]
   
   const moveOCheck = [...attemptMove]
+  
   
   moveXCheck.push('x')
   moveOCheck.push('o')
@@ -49,7 +50,7 @@ const performMove = async ({ gameId, user, changedRow, changedCol }) => {
     },
   
     UpdateExpression: `SET lastMoveBy = :user, ${rowInd} = ${rowInd} + ${rowVal}, ${colInd} = ${colInd} + ${colVal}, ${firstDiag} = ${newDiag}, ${secondDiag} = ${calcDiag}, currentValue[0] = currentValue[1], currentValue[1] = currentValue[0], currMove[0] = list_append(:newMove, currentValue[0][0]), previousMoves = list_append(previousMoves, currMove), diagSum[0] = diagSum[1], diagSum[1] = diagSum[0]`,
-    ConditionExpression: `(user1 = :user OR user2 = :user) AND lastMoveBy <> :user AND (:absRow <= game_size AND :absCol <= game_size) AND NOT (:xCheck = currMove[0] OR :oCheck = currMove[0]) AND NOT (contains(previousMoves, :xCheck) OR contains(previousMoves, :oCheck))`,
+    ConditionExpression: `active = :one AND (user1 = :user OR user2 = :user) AND lastMoveBy <> :user AND (:absRow <= game_size AND :absCol <= game_size) AND NOT (:xCheck = currMove[0] OR :oCheck = currMove[0]) AND NOT (contains(previousMoves, :xCheck) OR contains(previousMoves, :oCheck))`,
     
     ExpressionAttributeValues: {
       ':user': user,
@@ -58,8 +59,7 @@ const performMove = async ({ gameId, user, changedRow, changedCol }) => {
       ':newMove': attemptMove,
       ':xCheck': moveXCheck,
       ':oCheck': moveOCheck,
- 
-
+      ':one': 1
     },
     ReturnValues: 'ALL_NEW'
   }
