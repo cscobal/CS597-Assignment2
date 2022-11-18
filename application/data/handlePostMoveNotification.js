cbbf3890f628ac2f4c4e5c721eca9935c0ffd284
cscobal <cscobal@gmail.com>
2022-11-17 23:43:46 -0800

    Update handlePostMoveNotification.js

--- application/data/handlePostMoveNotification.js
+++ application/data/handlePostMoveNotification.js
@@ -10,14 +10,14 @@ const handlePostMoveNotification = async ({ game, mover, opponent }) => {
   const winAmt = (game.size * (game.size + 1)) / 2;
   for(let i = 0; i < game.size; i++)
   {
-    if(!win && (Math.abs(game.row[i]) == winAmt || Math.abs(game.col[i]) == winAmt))
+    if(!win && (Math.abs(game.tttRow[i]) == winAmt || Math.abs(game.tttCol[i]) == winAmt))
     {
       win = true
     }
   }
   for(let i = 0; i < 2; i++)
   {
-    if(!win && Math.abs(game.diag[i]) == winAmt)
+    if(!win && Math.abs(game.tttDiag[i]) == winAmt)
     {
       win = true
     }
@@ -26,15 +26,15 @@ const handlePostMoveNotification = async ({ game, mover, opponent }) => {
     const winnerMessage = `You beat ${mover.username} in a game of tic-tac-toe!`
     const loserMessage = `Ahh, you lost to ${opponent.username} in tic-tac-toe.`
     await Promise.all([
-      sendMessage({ email: opponent.email, message: winnerMessage }),
-      sendMessage({ email: mover.email, message: loserMessage })
+      sendMessage({ email: opponent.email, message: winnerMessage, subject: "losing" }),
+      sendMessage({ email: mover.email, message: loserMessage, subject: "winning" })
     ])
 
     return
   }
 
   const message = `${mover.username} has moved. It's your turn next in Game ID ${game.gameId}!`
-  await sendMessage({ email: opponent.email, message })
+  await sendMessage({ email: opponent.email, message: message, subject: "your turn" })
 };
 
 module.exports = handlePostMoveNotification;
