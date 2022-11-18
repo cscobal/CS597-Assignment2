ebbbd5092afe77eead56b91fc2907662cbd4e820
cscobal <cscobal@gmail.com>
2022-11-17 16:05:14 -0800

    Update create-lambda.sh

--- scripts/create-lambda.sh
+++ scripts/create-lambda.sh
@@ -7,7 +7,7 @@ zip -rq application.zip application/
 
 echo "Creating IAM role"
 ROLE_ARN=$(aws iam create-role \
-  --role-name Cloud9-turn-based-api-lambda-role \
+  --role-name Cloud9-tic-tac-toe-api-lambda-role \
   --assume-role-policy-document '{
       "Version": "2012-10-17",
       "Statement": [
@@ -25,7 +25,7 @@ ROLE_ARN=$(aws iam create-role \
 
 echo "Adding policy to IAM role"
 POLICY=$(aws iam put-role-policy \
-  --role-name Cloud9-turn-based-api-lambda-role \
+  --role-name Cloud9-tic-tac-toe-api-lambda-role \
   --policy-name lambda-policy \
   --policy-document '{
     "Version": "2012-10-17",
@@ -61,8 +61,8 @@ echo "Sleeping for IAM role propagation"
 sleep 10
 echo "Creating Lambda function"
 FUNCTION_ARN=$(aws lambda create-function \
-  --function-name turn-based-api \
-  --runtime nodejs10.x \
+  --function-name tic-tac-toe-api \
+  --runtime nodejs16.x \
   --role ${ROLE_ARN} \
   --handler application/handler.handler \
   --timeout 12 \
